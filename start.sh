0411287a28bbf89ad57dd03db37ebe104a34aded
Tom Powell <tom@powell.io>
2024-02-06 21:05:32 -0800

    use as http proxy instead of nfs, fixup tailscale

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=fly-www
-
-mount -t nfs 100.75.3.66:/data/www /var/www
+tailscale up --auth-key=${TAILSCALE_OAUTHKEY} --advertise-tags=tag:fly
 
 caddy run --config=/etc/caddy/Caddyfile
